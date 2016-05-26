0648fe8b08568d6ef3c0c660fcbe5cdc1cf520ac
tomerhp <tomerhp@gmail.com>
2016-05-26 15:16:32 +0300

    26.5 udpated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## When using this function we need to assign it to a variable. 
+## For testing: Mat<-matrix(sample(1:2),3,3)  
+## Temp_Mat<-makeCahseMatrix(mat)
+## cacheSolve(Temp_Mat) -> we get the result of solve(mat)
+## the second time we use this command: cacheSolve(Temp_Mat)  -> we get "getting cached data" and the result. which proves that there was no exess caclucation
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
 
+cacheSolve <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setmean(m)
+        m
 }
 
 
-## Write a short comment describing this function
+#Examples
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
